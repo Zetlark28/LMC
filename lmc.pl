de8aca5da33f91bd7c5f8b6c63709c333389a53d
Zetlark28 <39433171+Zetlark28@users.noreply.github.com>
2018-11-28 20:45:02 +0100

    Update lmc.pl

--- lmc.pl
+++ lmc.pl
@@ -57,12 +57,41 @@ one_instruction(State, NewState):-  State=..L,
                                     Cell is Istr-200,
                                     nth0(Cell, Mem, Val),
                                     nth0(1, L, Acc),
-                                    Ris is Acc-Val,
-                                    Ris >0,
+                                    sottrazione(Acc, Val, Ris),
+                                    Ris>0,
                                     Pc_agg is Pc+1,
                                     nth0(4, L, In),
                                     nth0(5, L, Out),
                                     NewState=.. [state, Ris, Pc_agg, Mem, In, Out, noflag].
+
+%%sub con flag
+one_instruction(State, NewState):- State=..L,
+                                    nth0(0, L, state),
+                                    nth0(3, L, Mem),
+                                    nth0(2, L, Pc),
+                                    nth0(Pc, Mem, Istr),
+                                    Istr>199,
+                                    Istr<300,
+                                    Cell is Istr-200,
+                                    nth0(Cell, Mem, Val),
+                                    nth0(1, L, Acc),
+                                    sottrazione(Acc, Val, Ris),
+                                    Ris<0,
+                                    Pc_agg is Pc+1,
+                                    nth0(4, L, In),
+                                    nth0(5, L, Out),
+                                    NewState=.. [state, Ris, Pc_agg, Mem, In, Out, flag].
+
+sottrazione(N1, N2, Ris):- Diff is N1-N2,
+                           Diff<(-1000),
+                           Ris is Diff+1000.
+
+sottrazione(N1, N2, Diff):- Diff is N1-N2,
+                            between(-1000, 1000, Diff).
+
+
+                                    
+                                    
                                     
 %%codice per lo store    
 
