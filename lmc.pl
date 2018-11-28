8af89feab36e99c161994e1ceebfb4e2b0e961ff
Zetlark28 <39433171+Zetlark28@users.noreply.github.com>
2018-11-28 11:24:26 +0100

    Add files via upload

---
+++ lmc.pl
@@ -0,0 +1,15 @@
+%%nome e matricola di ogni membro
+
+state(Acc, Pc, Mem, In, Out, Flag).
+
+halted_state(Acc, Pc, Mem, In, Out, Flag).
+
+one_instruction(State, NewState).
+
+execution_loop(State, Out).
+
+lmc_load(Filename,Mem).
+
+lmc_run(Filename, Input, Output):- lmc_load(Filename, Mem),
+
+                                   exectution_loop(State, Output).
