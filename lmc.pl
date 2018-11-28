8a73818bb78559cc22f823576b21c006eaff5441
Zetlark28 <39433171+Zetlark28@users.noreply.github.com>
2018-11-28 15:51:25 +0100

    Update lmc.pl

--- lmc.pl
+++ lmc.pl
@@ -4,7 +4,24 @@ state(Acc, Pc, Mem, In, Out, Flag).
 
 halted_state(Acc, Pc, Mem, In, Out, Flag).
 
-one_instruction(State, NewState).
+%%sub
+one_instruction(State, NewState):-  State=..L,
+                                    nth0(0, L, state),
+                                    nth0(3, L, Mem),
+                                    nth0(2, L, Pc),
+                                    nth0(Pc, Mem, Istr),
+                                    Istr>199,
+                                    Istr<300,
+                                    Cell is Istr-200,
+                                    nth0(Cell, Mem, Val),
+                                    nth0(1, L, Acc),
+                                    Ris is Acc-Val,
+                                    Ris >0,
+                                    Pc_agg is Pc+1,
+                                    nth0(4, L, In),
+                                    nth0(5, L, Out),
+                                    NewState=.. [state, Ris, Pc_agg, Mem, In, Out, noflag].
+
 
 execution_loop(State, Out).
 
